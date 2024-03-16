b251bd62254f76029ed3fd5a33a6149396a2b0d0
Miraga99 <miraga041@gmail.com>
2024-03-16 11:55:46 +0400

    Success

--- Garage-Homework-Class/Program.cs
+++ Garage-Homework-Class/Program.cs
@@ -1,20 +1,25 @@
-﻿
-
-using Garage_Homework_Class.Models;
+﻿using Garage_Homework_Class.Models;
 
 
-Group group = new Group();
-Console.WriteLine("Add a group number");
-group.GroupNo = int.Parse(Console.ReadLine());
-Console.WriteLine("How many student will be in the group");
-group.GroupLimit = int.Parse(Console.ReadLine());
-for (int i = 0; i < group.GroupLimit; i++)
-{
-    Student student = new Student();
-    group.AddStudent(student);
-    Array.Resize(ref group.students, group.students.Length+1);
-    group.students[group.students.Length - 1] = student;
-    Console.WriteLine("Student Added");
-}
+//Book book = new Book();
 
-group.GetStudent();
+//book.CreateBook();
+//book.ShowBook();
+
+
+//Group group = new Group();
+//Console.WriteLine("Add a group number");
+//group.GroupNo = int.Parse(Console.ReadLine());
+//Console.WriteLine("How many student will be in the group");
+//group.GroupLimit = int.Parse(Console.ReadLine());
+//for (int i = 0; i < group.GroupLimit; i++)
+//{
+//    Student student = new Student();
+//    group.AddStudent(student);
+//    Array.Resize(ref group.students, group.students.Length+1);
+//    group.students[group.students.Length - 1] = student;
+//    Console.WriteLine("Student Added");
+//}
+
+//group.GetStudent();
+
